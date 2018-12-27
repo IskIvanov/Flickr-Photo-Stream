a09c127c13e6b3d3b1ddd5d5bd9e6b95744fe0df
Iskren Ivanov <iskren93@gmail.com>
2018-12-27 13:35:35 +0200

    components added

---
+++ src/Title/Title.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Author from './Author';
+
+export default class Title extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+           
+        <Author />
+      </div>
+    )
+  }
+}
---
+++ src/flickrImage/FlickrImage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class FlickrImage extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
